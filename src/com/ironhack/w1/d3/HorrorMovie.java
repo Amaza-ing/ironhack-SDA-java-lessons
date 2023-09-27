0d2629a2df763bb260e58407eba0b384ac40c457
Adrián Maza <adri_mv1@hotmail.com>
2023-09-27 11:16:45 +0200

    classes & inheritance examples created

---
+++ src/com/ironhack/w1/d3/HorrorMovie.java
@@ -0,0 +1,30 @@
+package com.ironhack.w1.d3;
+
+public class HorrorMovie extends Movie {
+    private boolean isReallyScary;
+
+    public HorrorMovie(String title, int duration, boolean isReallyScary) {
+        super(title, duration);
+        this.isReallyScary = isReallyScary;
+    }
+
+    public boolean isReallyScary() {
+        return isReallyScary;
+    }
+
+    public void setReallyScary(boolean reallyScary) {
+        isReallyScary = reallyScary;
+    }
+
+    @Override
+    public String toString() {
+        return "HorrorMovie{" +
+                super.toString() + " " +
+                "isReallyScary=" + isReallyScary +
+                '}';
+    }
+
+    public void play() {
+        System.out.println("playing a horror movie");
+    }
+}
---
+++ src/com/ironhack/w1/d3/Main.java
@@ -0,0 +1,22 @@
+package com.ironhack.w1.d3;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie harryPotter = new Movie("Harry Potter", 120);
+        Movie lotr = new Movie("Lord of the Rings", 200);
+        Movie madMax = new Movie("Mad Max");
+
+        System.out.println(harryPotter.getTitle() + " - " + harryPotter.getDuration());
+        System.out.println(lotr.getTitle() + " - " + lotr.getDuration());
+        System.out.println(harryPotter);
+        System.out.println(harryPotter.IS_GOOD);
+        System.out.println(madMax.getTitle() + " - " + madMax.getDuration());
+        harryPotter.play();
+        Movie.displayAlert();
+
+        HorrorMovie scream = new HorrorMovie("Scream", 80, false);
+        System.out.println(scream.getTitle() + " - " + scream.getDuration() + " - " + scream.isReallyScary());
+        scream.play();
+        System.out.println(scream);
+    }
+}
