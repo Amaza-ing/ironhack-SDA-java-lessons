c076bb9204e5fa63b527cdaca2902a19f7d46cb6
Adrián Maza <adri_mv1@hotmail.com>
2023-09-27 09:23:23 +0200

    menu created

---
+++ src/com/ironhack/w1/d3/Menu.java
@@ -0,0 +1,11 @@
+package com.ironhack.w1.d3;
+
+public class Menu {
+    public static void main(String[] args) {
+        System.out.println("something something");
+        System.out.println("something something");
+        System.out.println("something something");
+        System.out.println("something something");
+        System.out.println("something something");
+    }
+}
